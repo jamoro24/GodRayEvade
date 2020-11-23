c8161861cfc85573ca033a43152ea915f5960e5a
Andres Gomez <and-gome@uniandes.edu.co>
2020-11-24 06:37:47 +0900

    Add default spawn positions and fix references errors
    Created default position for cameras, players and weapons.
    Fixed missed references of billboard and slider of player.

--- GodRayEvade/Assets/Scripts/Billboard.cs
+++ GodRayEvade/Assets/Scripts/Billboard.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    public Transform cam;
+    private Transform cam;
     void LateUpdate()
     {
+        cam = GameObject.Find("MainCamera").transform;
         transform.LookAt(transform.position + cam.forward);
     }
 }
