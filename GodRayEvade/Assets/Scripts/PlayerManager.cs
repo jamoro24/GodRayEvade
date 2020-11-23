179a6e56c71259f6be1c096c8e70420957747e98
Andres Gomez <and-gome@uniandes.edu.co>
2020-11-24 07:05:50 +0900

    Instantiate players and cameras, add spot light
    Players and cameras are instantiated in correct positions.
    The spot lights were added to show where the user is looking at.
    Color of floor changed to white to be able to see the light.

---
+++ GodRayEvade/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,52 @@
+﻿using MLAPI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : NetworkedBehaviour
+{
+    private GameObject mainCamera;
+    private GameObject camera1Pos;
+    private GameObject camera2Pos;
+    private GameObject player1Pos;
+    private GameObject player2Pos;
+    private GameObject player1Weapon;
+    private GameObject player2Weapon;
+    public GameObject spotlightPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("MainCamera");
+        camera1Pos = GameObject.Find("Camera1Pos");
+        camera2Pos = GameObject.Find("Camera2Pos");
+        player1Pos = GameObject.Find("Player1Pos");
+        player2Pos = GameObject.Find("Player2Pos");
+        player1Weapon = GameObject.Find("Player1Weapon");
+        player2Weapon = GameObject.Find("Player2Weapon");
+        if (IsServer)
+        {
+            transform.position = player1Pos.transform.position;
+            mainCamera.transform.position = camera1Pos.transform.position;
+            mainCamera.transform.rotation = camera1Pos.transform.rotation;
+        }
+        else
+        {
+            transform.position = player2Pos.transform.position;
+            mainCamera.transform.position = camera2Pos.transform.position;
+            mainCamera.transform.rotation = camera2Pos.transform.rotation;
+        }
+
+        if (IsServer)
+        {
+            GameObject go = Instantiate(spotlightPrefab, Vector3.zero, Quaternion.identity);
+            go.GetComponent<NetworkedObject>().SpawnWithOwnership(GetComponent<NetworkedObject>().OwnerClientId);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ GodRayEvade/Assets/Scripts/SpotLightManager.cs
@@ -0,0 +1,45 @@
+﻿using MLAPI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLightManager : NetworkedBehaviour
+{
+    private GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IsOwner)
+        {
+            mainCamera = GameObject.Find("MainCamera");
+            transform.position = mainCamera.transform.position;
+
+        }
+
+        if (IsServer)
+        {
+            if (IsOwner)
+                GetComponentInChildren<Light>().color = new Color(1f, 0f, 0f);
+            else
+                GetComponentInChildren<Light>().color = new Color(0f, 1f, 0f);
+        }
+        else
+        {
+            if (IsOwner)
+                GetComponentInChildren<Light>().color = new Color(0f, 1f, 0f);
+            else
+                GetComponentInChildren<Light>().color = new Color(1f, 0f, 0f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsOwner)
+        {
+            transform.rotation = mainCamera.transform.rotation;
+        }
+
+    }
+}
